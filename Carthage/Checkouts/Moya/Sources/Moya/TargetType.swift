3e459d56f0e167cb9f417ca6b5b3ae3bd5107eb3
Andrew Vyazovoy <andrew.vyazovoy@gmail.com>
2017-09-02 02:53:21 +0300

    Add Carthage dependency management

---
+++ Carthage/Checkouts/Moya/Sources/Moya/TargetType.swift
@@ -0,0 +1,66 @@
+import Foundation
+
+/// The protocol used to define the specifications necessary for a `MoyaProvider`.
+public protocol TargetType {
+
+    /// The target's base `URL`.
+    var baseURL: URL { get }
+
+    /// The path to be appended to `baseURL` to form the full `URL`.
+    var path: String { get }
+
+    /// The HTTP method used in the request.
+    var method: Moya.Method { get }
+
+    /// Provides stub data for use in testing.
+    var sampleData: Data { get }
+
+    /// The type of HTTP task to be performed.
+    var task: Task { get }
+
+    /// Whether or not to perform Alamofire validation. Defaults to `false`.
+    var validate: Bool { get }
+
+    // The headers to be used in the request.
+    var headers: [String: String]? { get }
+}
+
+public extension TargetType {
+    var validate: Bool {
+        return false
+    }
+}
+
+/// Represents an HTTP task.
+public enum Task {
+
+    /// A request with no additional data.
+    case requestPlain
+
+    /// A requests body set with data.
+    case requestData(Data)
+
+    /// A requests body set with encoded parameters.
+    case requestParameters(parameters: [String: Any], encoding: ParameterEncoding)
+
+    /// A requests body set with data, combined with url parameters.
+    case requestCompositeData(bodyData: Data, urlParameters: [String: Any])
+
+    /// A requests body set with encoded parameters combined with url parameters.
+    case requestCompositeParameters(bodyParameters: [String: Any], bodyEncoding: ParameterEncoding, urlParameters: [String: Any])
+
+    /// A file upload task.
+    case uploadFile(URL)
+
+    /// A "multipart/form-data" upload task.
+    case uploadMultipart([MultipartFormData])
+
+    /// A "multipart/form-data" upload task  combined with url parameters.
+    case uploadCompositeMultipart([MultipartFormData], urlParameters: [String: Any])
+
+    /// A file download task to a destination.
+    case downloadDestination(DownloadDestination)
+
+    /// A file download task to a destination with extra parameters using the given encoding.
+    case downloadParameters(parameters: [String: Any], encoding: ParameterEncoding, destination: DownloadDestination)
+}
